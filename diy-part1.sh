6ec448c026b4587869e7895bf56062b7af2a7386
VOVO518 <129350523+VOVO518@users.noreply.github.com>
2023-06-24 02:20:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,19 +16,9 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-# git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced 
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
-rm -rf package/lean/autocore
-git clone https://github.com/sirpdboy/myautocore package/myautocore
-rm -rf package/lean/autosamba
-git clone https://github.com/sirpdboy/autosamba package/autosamba
+
 
 # echo '添加jerrykuku的argon-mod主题'
 # rm -rf package/lean/luci-theme-argon  
